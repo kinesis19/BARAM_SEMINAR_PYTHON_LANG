029bff548cce03a053fef0bcba55b60d6d05e118
Kinesis <lordkinesis@naver.com>
2024-04-08 20:00:40 +0900

    Adding exam10.py

---
+++ Day1/exam10.py
@@ -0,0 +1,15 @@
+condition1 = False
+condition2 = True
+
+if condition1 and condition2:
+    print("True")
+else:
+    print("False")
+
+
+print("---")
+
+if condition1 or condition2:
+    print("True")
+else:
+    print("False")
