8c7a85299b08452fbaa7032f47f0b7669d7f4804
Kinesis <lordkinesis@naver.com>
2024-04-08 19:35:38 +0900

    Adding exam02.py, exam03.py

---
+++ Day1/exam02.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+fruits = ['apple', 'banana', 'orange']
+print(fruits)
+
+mix = [1, 'apple', 5, -1, True, False]
+print(mix)
+
---
+++ Day1/exam03.py
@@ -0,0 +1,12 @@
+numbers = (1, 2, 3, 4, 5)
+print(numbers)
+
+fruits = ('apple', 'banana', 'organge')
+print(fruits)
+
+mix = (1, 'apple', 5, -1, True, False)
+print(mix)
+
+# Tuple Error
+mix[1] = 'banana'
+print(mix[1])
