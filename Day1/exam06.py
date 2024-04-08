72d40138550a8280992ac89086870d9d5e5d4b04
Kinesis <lordkinesis@naver.com>
2024-04-08 19:54:06 +0900

    Adding exam06.py

---
+++ Day1/exam06.py
@@ -0,0 +1,17 @@
+# Index Example
+string = "Hello, World!"
+print(string[0])
+print(string[-1])
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[3])
+print(numbers[-3])
+
+# Slicing Example
+string = "hello, World!"
+print(string[0:5])
+print(string[7:])
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[:])
+print(numbers[-3:])
