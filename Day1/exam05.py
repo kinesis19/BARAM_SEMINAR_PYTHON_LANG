5814516433c13e81bff2e944b82217676664732f
Kinesis <lordkinesis@naver.com>
2024-04-08 19:45:36 +0900

    Adding exam05.py

---
+++ Day1/exam05.py
@@ -0,0 +1,7 @@
+person = {'name' : 'john', 'age' : 30, 'city' : 'New York'}
+print(person)
+print(person['age'])
+
+student = {'name' : 'Alice', 'age' : 25, 'city' : 'London'}
+print(student)
+print(student['age'])
